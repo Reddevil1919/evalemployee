3976cf5805b1921eae8b4cc91a0c75292c504b1f
Ahmed <Marzouk.Ahmed.Ensi@gmail.com>
2021-12-15 00:39:21 +0100

    Add ID Generator TASK_SEQ

--- spring-boot-rest/src/main/java/model/Criteria.java
+++ spring-boot-rest/src/main/java/model/Criteria.java
@@ -1,16 +1,22 @@
 package model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 @Entity
+@SequenceGenerator(name = "TASK_SEQ",sequenceName = "TASK_SEQ")
 @Getter @Setter @NoArgsConstructor
 public class Criteria {
 
-	@Id private String label;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "TASK_SEQ")
+	private String label;
 	private Integer rating;
 	private Integer coefficient;
 	
