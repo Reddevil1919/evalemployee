c8cda0b24a8cbab410df43db26af7b01b2113578
Ahmed <Marzouk.Ahmed.Ensi@gmail.com>
2021-12-15 23:58:04 +0100

    Add getting task by ID

--- spring-boot-rest/src/main/java/restservices/TaskRestService.java
+++ spring-boot-rest/src/main/java/restservices/TaskRestService.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,6 +54,16 @@ public class TaskRestService {
 			return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
+	}
+	@GetMapping("/tasks/{id}")
+	public ResponseEntity<Task> getTaskById(@PathVariable("id") long id)
+	{
+		try {
+			return new ResponseEntity<Task>(taskRepository.findById(id).get(),HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 	@RequestMapping("/")
